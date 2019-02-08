ba0fb502ef982c534b6331e7a5d49db17df63969
Ewen Earle <ewen_earle@hotmail.com>
2019-02-08 17:02:07 -0330

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# OOP Game Show App
+
+<a href="https://teamtreehouse.com" target="_blank">Team Treehouse</a> Full Stack Techdegree - Project #4
+
+## Introduction
+
+* Javascript and minor css styling created by Ewen Earle
+* HTML and CSS provided by Team Treehouse
+
+## Additions
+
+* A fade in method for the end game screen
+* A warning message for last heart
+* Body and text colors were changed from the default
+
+
---
+++ js/app.js
@@ -0,0 +1,26 @@
+let game;
+
+// Create new game object and start game on button click
+document.getElementById('btn__reset').addEventListener('click', () => {
+    game = new Game();  
+    game.startGame();   
+})
+
+
+// Listen for virtual keyboard event
+document.addEventListener('click', (e) => {
+    let letter = e.target.textContent;
+    if (e.target.value === '') game.handleInteraction(letter);  
+});
+
+
+// Listen for physical keyboard event once game is visible
+document.addEventListener('keypress', (e) => {
+    if(document.getElementById('overlay').style.visibility == 'hidden'){
+        let letter = String.fromCharCode(e.keyCode);
+        var reg = /[a-zA-Z]+/;
+        if (reg.test(letter)) game.handleInteraction(letter);
+    };
+ });
+
+
